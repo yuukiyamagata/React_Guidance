8b9d19769e2f8f2b7fd6ed5e9c69a32fbe43d011
yuki yamagata <yshanxing4@gmail.com>
2022-06-17 01:02:45 +0900

    add: api追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Counter } from './components/Counter';
 
 
 export const App = () => {
   return(
     <>
-    <div className='ui container' style={{ marginTop: '20px'}}>
-      <Counter />
-    </div>
     </>
   )
 }
